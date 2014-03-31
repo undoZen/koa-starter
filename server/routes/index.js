a562b17ef3f955d70708ca8eb264df9bcf6acd98
Anson Yu Wang <opensource@undozen.com>
2014-03-31 19:45:03 +0800

    add fast static prototyping utility

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,26 @@
+'use strict';
+
+// For fast static prototyping, remove if you don't need it anymore
+//*/
+app.use(function *(next) {
+  yield next;
+  var path = require('path');
+  var parse = require('co-body');
+
+  var viewPath = path.join(__dirname, '..', 'views', this.path + '.jade');
+  var viewExists = yield L.exists(viewPath);
+  if (!this.body && viewExists) {
+    if (this.idempotent) {
+      this.type = 'html';
+    } else {
+      var body = yield parse(this);
+      this.type = 'json';
+      this.body = body;
+    }
+  }
+});
+//*/
+
 app.use(route.idpt('/', function *(next) {
   this.type = 'html';
 }));
